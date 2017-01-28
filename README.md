c41893924223ef00463ca4e8cf3f8586d693c4fe
Eric Lavigne <lavigne.eric@gmail.com>
2017-01-28 18:49:06 -0500

    First coherent version of model.py.

--- README.md
+++ README.md
@@ -2,3 +2,36 @@
 
 Explains the structure of your network and training approach,
 including complete descriptions of the problems and the strategies.
+
+## Training
+
+## Running the Simulator
+
+## Installation (using VirtualEnv)
+
+First time:
+
+```
+virtualenv env
+source env/bin/activate
+pip install -r requirements.txt
+deactivate
+```
+
+When starting to work:
+
+```
+source env/bin/activate
+```
+
+After installing new libraries:
+
+```
+pip freeze > requirements.txt
+```
+
+When finished:
+
+```
+deactivate
+```
--- model.py
+++ model.py
@@ -5,6 +5,9 @@ import base64
 import json
 
 import numpy as np
+import pandas as pd
+import matplotlib.image as mpimg
+import cv2
 
 import time
 from PIL import Image
@@ -14,15 +17,19 @@ from io import BytesIO
 
 from keras.models import model_from_json
 from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array
+from keras.layers.core import Dense, Activation, Flatten, Dropout
+from keras.layers.convolutional import Convolution2D
 
 import tensorflow as tf
 tf.python.control_flow_ops = tf # mysterious fix to keras/tensorflow issue
 
-image = Image.open(BytesIO(base64.b64decode(imgString)))
-image_array = np.asarray(image)
-transformed_image_array = image_array[None, :, :, :]
+# Image processing snippets from drive.py
+# image = Image.open(BytesIO(base64.b64decode(imgString)))
+# image_array = np.asarray(image)
+# transformed_image_array = image_array[None, :, :, :]
 
-steering_angle = float(model.predict(transformed_image_array, batch_size=1))
+# Model prediction snippet from drive.py
+# steering_angle = float(model.predict(transformed_image_array, batch_size=1))
 
 # Later I'll probably add speed as an input variable and
 # acceleration as an output variable.  This will involve
@@ -35,34 +42,81 @@ steering_angle = float(model.predict(transformed_image_array, batch_size=1))
 # to throttle = -0.2 to check if that works. If not, will need to
 # ask Udacity staff if simulator can be upgraded.
 
+steering_bins = [-1, -0.4, -0.15, 0.0, 0.15, 0.4, 1]
+throttle_bins = [-1, 0, 1]
 
+def convert_steer_angle_to_bin(angle):
+  best_bin = None
+  best_diff = 10
+  for i,bin_angle in enumerate(steering_bins):
+    diff = abs(angle - bin_angle)
+    if diff < best_diff:
+      best_diff = diff
+      best_bin = i
+  return best_bin
+
+# import model as m
+# m.load_image('/Users/ericlavigne/workspace/CarND-Simulator/IMG/center_2017_01_21_19_10_57_316.jpg')
+
+def load_image(file_name):
+  img = mpimg.imread(file_name)
+  img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+  return img
+
+# import model as m; m.load_summary_data('../CarND-Simulator').head(5)
+# Result is DataFrame: http://pandas.pydata.org/pandas-docs/stable/api.html#dataframe
+
+def load_summary_data(data_dir):
+  file_name = data_dir + '/driving_log.csv'
+  col_names = ['img_center','img_left','img_right','steer','throttle','brake','speed']
+  df = pd.read_csv(file_name, names=col_names)
+  df['steer_bin'] = df['steer'].apply(lambda angle: convert_steer_angle_to_bin(angle))
+  df.drop('img_left', 1, inplace=True)
+  df.drop('img_right', 1, inplace=True)
+  return df
+
+def create_model():
+  model = Sequential()
+  # Convolution2D(output_depth, convolution height, convolution_width, ...)
+  model.add(Convolution2D(10, 5, 5, border_mode='valid', input_shape=(160,320,3)))
+  # model.add(Dropout(0.5)
+  model.add(Flatten())
+  model.add(Dense(32))
+  model.add(Dense(len(steering_bins), activation='softmax'))
+  model.compile(optimizer='adam',
+                loss='categorical_crossentropy',
+                metrics=['accuracy'])
+  return model
+
+def train_model(model, data_dir):
+  pass
 
 # Saving and loading keras models
 # https://keras.io/models/about-keras-models/
 
-model.summary()
-model.get_config()
-model = Model.from_config(config)
+# model.summary()
+# model.get_config()
+# model = Model.from_config(config)
+  
+def save_model(model,path='model'):
+  with open(path + '.json', 'w') as arch_file:
+    arch_file.write(model.to_json())
+  model.save_weights(path + '.h5')
 
-from models import model_from_json
-
-json_string = model.to_json()
-model = model_from_json(json_string)
-
-with open(args.model, 'r') as jfile:
-  model = model_from_json(jfile.read())
-
-weights_file = args.model.replace('json', 'h5')
-model.load_weights(weights_file)
-
-# Main
-
-# Args could include training data folder and output model file.
+def load_model(path):
+  with open(path + '.json', 'r') as arch_file:
+    model = model_from_json(arch_file.read())
+    model.load_weights(path + '.h5')
+    return model
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Remote Driving')
-    parser.add_argument('model', type=str,
-    help='Path to model definition json. Model weights should be on the same path.')
+    parser = argparse.ArgumentParser(description='SimDrive Training')
+    parser.add_argument('load_model', type=str,
+                        help='Path to model definition for loading (without json/h5 extension)')
+    parser.add_argument('save_model', type=str,
+                        help='Path to model definition for saving (without json/h5 extension)')
+    parser.add_argument('training_data', type=str,
+                        help='Path to folder with driving_log.csv and IMG subfolder')
     args = parser.parse_args()
 
-    
+    print(args.model)    
